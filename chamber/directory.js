2224d8adcfe7876dbfc897189478e823f2a18824
pius-boy <godwinunimashi.pius@gmail.com>
2025-11-16 06:36:16 +0100

    edited

--- chamber/directory.js
+++ chamber/directory.js
@@ -5,24 +5,37 @@ document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
 
 // Mobile menu toggle
-const toggleBtn= Document.getElementById('menu-toggle');
+const toggleBtn = document.getElementById('menu-toggle');
 const navmenu = document.getElementById('nav-menu');
 
-toggleBtn.addEventlistener('click', () => {
-    navmenu.classList.toggle('show');
+toggleBtn.addEventListener('click', () => {
+    navmenu.classList.toggle('open'); 
 });
+
 
 // Grid and list view toggle
 const members = document.getElementsByClassName('members-card');
 const gridBtn = document.getElementById('gridbtn');
 const listBtn = document.getElementById('listbtn');
 
+// GRID VIEW
 gridBtn.addEventListener('click', () => {
-    members.classList.add('grid-view');
-    members.classList.remove('list-view');
+    for (let card of members) { // members is like an array,You must loop through it:
+        card.classList.remove('list-view');
+        card.classList.add('grid-view');
+    }
+
+    gridBtn.classList.add('active');
+    listBtn.classList.remove('active');
 });
 
+// LIST VIEW
 listBtn.addEventListener('click', () => {
-    members.classList.add('list-view');
-    members.classList.remove('grid-view');
+    for (let card of members) {
+        card.classList.remove('grid-view');
+        card.classList.add('list-view');
+    }
+
+    listBtn.classList.add('active');
+    gridBtn.classList.remove('active');
 });
